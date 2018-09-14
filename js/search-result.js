92c94d0dee67d690f78f6b27ccdf2ae9d800756a
leena <623360853@qq.com>
2018-09-14 23:15:43 +0800

    实现了search-result的分页功能

--- js/search-result.js
+++ js/search-result.js
@@ -1,21 +1,23 @@
+// 获取地址栏中用户输入的关键字
+var keyword = getParamsByUrl(location.href, 'keyword');
+// 当前页
+var page = 1;
+var html = '';
 $(function() {
-    var keyword = getParamsByUrl(location.href, 'keyword');
 
-    $.ajax({
-        url: '/product/queryProduct',
-        type: 'get',
-        data: {
-            page: 1,
-            pageSize: 6,
-            proName: keyword
-        },
-        success:function(res){
-            console.log(res);
-            var html = template('searchTpl',res);
-            console.log(html);
-            $('#search-box').html(html);
+    mui.init({
+        pullRefresh : {
+            container:'#refreshContainer',//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
+            up : {
+                height:50,//可选.默认50.触发上拉加载拖动距离
+                auto:true,//可选,默认false.自动上拉加载一次
+                contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
+                contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+                callback :getData //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+            }
         }
-    })
+    });
+
 
 
 })
@@ -37,3 +39,30 @@ function getParamsByUrl(url,name) {
     }
     return null;
 }
+
+function getData() {
+    var This = this;
+    $.ajax({
+        url: '/product/queryProduct',
+        type: 'get',
+        data: {
+            page: page++,
+            pageSize: 3,
+            proName: keyword
+        },
+        success:function(res){
+            console.log(res);
+            if(res.data.length > 0) {
+                html += template('searchTpl',res);
+                console.log(html);
+                $('#search-box').html(html);
+                // 告诉上拉加载组件当前数据加载完毕
+                This.endPullupToRefresh(false);
+            }else {
+                This.endPullupToRefresh(true);
+            }
+
+        }
+    });
+}
+
